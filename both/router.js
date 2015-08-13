aa5fe19e293fabb89f31f567a7f01e8bb90562c1
unknown <kettljw6@IM-LT10226.Immediate.Media>
2015-08-13 17:15:42 +0100

    BinShi changes for today added meteor ionic

---
+++ both/router.js
@@ -0,0 +1,18 @@
+Router.configure({
+  layoutTemplate: 'layout'
+});
+
+Meteor.startup(function () {
+  if (Meteor.isClient) {
+    var location = Iron.Location.get();
+    if (location.queryObject.platformOverride) {
+      Session.set('platformOverride', location.queryObject.platformOverride);
+    }
+  }
+});
+
+Router.map(function() {
+  this.route('index', {path: '/'});
+  this.route('find');
+  this.route('report');
+  });
